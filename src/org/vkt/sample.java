0da329525bb706b29db485789a2f4099ecd51e72
venkatcut <venkatsivam98@gmail.com>
2022-02-25 19:54:48 -0800

    done

---
+++ src/org/vkt/sample.java
@@ -0,0 +1,19 @@
+package org.vkt;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sample {
+	
+	       public static void main(String[] args) {
+	    	   System.setProperty("webdriver.driver.chromedriver", "C:\\Users\\ELCOT\\Downloads\\venkat\\lib\\driver\\chromedriver.exe");
+	    	   WebDriver driver = new ChromeDriver();
+	    	   
+	    	   
+	    	   
+			
+		}
+
+}
